0ee9bdb0782acef71fc1a589ff48b27a19ee23c6
mozzam123 <mozzam607@gmail.com>
2023-10-26 11:52:58 +0530

    added mongodb connection

--- Project/app/views.py
+++ Project/app/views.py
@@ -6,3 +6,26 @@ from rest_framework.views import APIView
 class TestView(APIView):
     def get(self, request):
         return JsonResponse({"message": "Its working"})
+
+
+
+
+import pymongo
+
+client = pymongo.MongoClient('mongodb+srv://mozzam:mozzam@latestcluster.wdjuvrc.mongodb.net/')
+dbname = client['Django']
+
+#Define Collection
+collection = dbname['mascot']
+
+mascot_1={
+    "name": "Sammy",
+    "type" : "Shark"
+}
+
+collection.insert_one(mascot_1)
+
+mascot_details = collection.find({})
+
+for r in mascot_details:
+    print(r['name'])
